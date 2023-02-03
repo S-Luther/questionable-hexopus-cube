baa8017bdf80076dc92f20f04a584060796817d5
Samuel Luther <samuelluther@SUU.EDU>
2023-02-03 11:44:12 -0700

    file change

--- src/main/java/com/example/project/Cube.java
+++ src/main/java/com/example/project/Cube.java
@@ -1,6 +1,6 @@
 package com.example.project;
 
-public class Hello {
+public class Cube {
 
 	public static void main(final String[] args) {
 		System.out.println("Not the right string, tests will fail!");
