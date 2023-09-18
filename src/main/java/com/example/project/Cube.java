51dd27a8e846339e3a8e11b0895f9753631bbd21
Sam Luther <samluther998@gmail.com>
2023-09-18 09:25:40 -0600

    Update Cube.java

--- src/main/java/com/example/project/Cube.java
+++ src/main/java/com/example/project/Cube.java
@@ -3,8 +3,6 @@ package com.example.project;
 public class Cube {
 
 	public static void main(final String[] args) {
-		System.out.println("Not the right string, tests will fail!");
-	//hi there
 	
 	}
 
